ec998f6b1c4f42c8fa524ce938ce5402ca6843bc
Noah Kennedy <Nomaxx117@gmail.com>
2018-05-01 11:46:55 -0500

    Trying to add serial

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [venantius/pyro "0.1.2"]
-                 [aleph "0.4.4"]
-                 [clj-serial "2.0.4-SNAPSHOT"]]
+                 [aleph "0.4.4"]]
   :main ^:skip-aot rmc-core-2018-2019.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/rmc_core_2018_2019/core.clj
+++ src/rmc_core_2018_2019/core.clj
@@ -2,8 +2,29 @@
   (:gen-class)
   (:require aleph.tcp))
 
+(declare arduino-serial-agent)
+
+(defn send-to-arduino [message])
+
+(def mode (atom :manual))
+
+(defmacro direct-send [command]
+  `(fn [~'message]
+     (send-to-arduino (str ~command ~'message))))
+
+(defn lookup-command-handler [command]
+  (get {"drv"  (direct-send command)
+        "intk" (direct-send command)
+        "auto" (fn [] (swap! mode :auto))
+        "man"  (fn [] (swap! mode :manual))}
+       command))
+
 (defn handle-incoming [message]
+  ())
+
+(defn print-incoming [message]
   (println message))
 
 (defn -main [& args]
   (aleph.tcp/start-server handle-incoming {:port 2401}))
+
