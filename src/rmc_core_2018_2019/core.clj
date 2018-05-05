cd8d973ee71dcbaf01c6d3dc780d8f383d926451
noah <nomaxx117@gmail.com>
2018-05-05 17:40:14 -0500

    Added docstrings

--- src/rmc_core_2018_2019/core.clj
+++ src/rmc_core_2018_2019/core.clj
@@ -13,17 +13,26 @@
 (pyro.printer/swap-stacktrace-engine!)
 (set! *warn-on-reflection* true)
 
-(def ^String arduino-com-port "COM5")
 
+(def ^String arduino-com-port "COM5")
 (declare arduino)
 
-(defn send-to-arduino [^String message]
+
+(defn send-to-arduino
+  "Sends a message over the serial interface to the Arduino."
+  [^String message]
   (-> ^CommPort arduino
       ^OutputStream .getOutputStream
       (PrintWriter. true)
       (.println message)))
 
-(defmacro defpass [name function]
+
+(defmacro defpass
+  "Defines a passthrough function which will take in the
+  entire command as an argument. This is good for
+  situations in which you will be relaying the
+  entire command forwards to another device."
+  [name function]
   `(defn ~name [^String ~'command]
      (fn [~'body]
        (~function
@@ -39,23 +48,45 @@
 
 (defpass direct-return (fn [message] message))
 
-(defn lookup-command-handler [^String command]
+(defn lookup-command-handler
+  "Looks up the command handler in a map using the command provided.
+  Returns back a command which will take in the arguments of the
+  command in vector form."
+  [^String command]
   (get {"drv"  (direct-send command)
         "test" (direct-return command)}
        command))
 
-(defn handle-new-message [^String message]
+(defn handle-new-message
+  "Handles a new incoming message that was sent by the GUI."
+  [^String message]
   (let [tokens (string/split message (Pattern/compile " "))
         command (first tokens)
         args (rest tokens)]
     ((lookup-command-handler command) args)))
 
-(defn handle-new-connection [stream _]
-  (s/consume (fn [bytes]
-               (handle-new-message
-                 (byte-streams/convert bytes String)))
-             stream))
-
+(defn handle-new-connection
+  "Handles a new connection request sent by a TCP client."
+  [stream _]
+  (do (letfn [(handle-arduino-message [message]
+                (s/put! stream message))]
+        (-> ^Runnable (s/consume
+                        (fn [bytes]
+                          (-> bytes
+                              (byte-streams/convert String)
+                              handle-arduino-message))
+                        (-> arduino
+                            .getInputStream))
+            Thread.
+            .start))
+      (-> ^Runnable (s/consume
+                      (fn [bytes]
+                        (-> bytes
+                            (byte-streams/convert String)
+                            handle-new-message))
+                      stream)
+          Thread.
+          .start)))
 
 (defn -main []
   (do
