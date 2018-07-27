6015d3054b4101cd941aa25d772fbbf5e4a9b5e1
Noah Kennedy <Nomaxx117@gmail.com>
2018-07-26 21:37:22 -0500

    Began implementing redesigned networking module

--- src/rmc_core_2018_2019/core.clj
+++ src/rmc_core_2018_2019/core.clj
@@ -1,147 +1,58 @@
 (ns rmc-core-2018-2019.core
-  (:gen-class)
-  (:require aleph.tcp
-            [clojure.string :as string]
-            [manifold.stream :as s]
-            pyro.printer)
-  (:import (java.util.regex Pattern)
-           (purejavacomm CommPortIdentifier CommPort)
-           (java.util Scanner)
-           (java.io PrintWriter)))
+   (:gen-class)
+   (:require aleph.tcp
+             [manifold.stream :as s]
+             pyro.printer))
 
 (set! *warn-on-reflection* true)
 (pyro.printer/swap-stacktrace-engine!)
 
-
-(def ^String arduino-com-port "COM6")
-
-(declare arduino-printer)
-(declare arduino-reader)
-
-(def is-alive (atom false))
-(def client-streams (ref []))
-
-(defn send-to-clients [message]
-  (dosync
-    (ensure client-streams)
-    (pmap (fn [stream-agent]
-            (send-off stream-agent
-                      (fn [stream]
-                        (do (s/put! stream message)))))
-          @client-streams)))
-
-(defn send-to-arduino
-  "Sends a message over the Arduino serial interface to the Arduino."
-  [^String message]
-  (when @is-alive
-    (send-off arduino-printer
-              (fn [^PrintWriter printer]
-                (do (.println printer message)
-                    printer)))))
-
-(defn print-from-arduino
-  "Prints out the most recently sent message from the arduino.
-  Used mainly for test code."
-  []
-  (send-off arduino-reader
-            (fn [^Scanner reader]
-              (do
-                (-> reader
-                    .nextLine
-                    println)
-                reader))))
-
-(defmacro defpass
-  "Defines a pass-through function which will take in the
-  entire command as an argument. This is good for
-  situations in which you will be relaying the
-  entire command forwards to another device."
-  [name function]
-  `(defn ~name [^String ~'command]
-     (fn [~'body]
-       (~function
-         (string/replace
-           (string/join
-             " "
-             (flatten
-               (list ~'command ~'body)))
-           "\n"
-           "")))))
-
-(defpass sendoff-to-arduino send-to-arduino)
-
-(defpass pass-print println)
-
-(defpass direct-return (fn [message]
-                         (if @is-alive
-                           message
-                           :dead)))
-
-(defn lookup-report-handler
-  [^String report]
-  (get {"test" (pass-print report)}
-       report))
-
-(defn handle-new-report [^String report]
-  (let [tokens (string/split report (Pattern/compile " "))
-        command (first tokens)
-        args (rest tokens)]
-    ((lookup-report-handler command) args)))
-
-(defn lookup-command-handler
-  "Looks up the command handler in a map using the command provided.
-  Returns back a command which will take in the arguments of the
-  command in vector form."
-  [^String command]
-  (get {"drv"  (sendoff-to-arduino command)
-        "test" (direct-return command)
-        "intk" (sendoff-to-arduino command)
-        "dmp"  (sendoff-to-arduino command)
-        "dsbl" (fn [_] (swap! is-alive (fn [_] false)))
-        "enbl" (fn [_] (swap! is-alive (fn [_] true)))
-        "mt"   (sendoff-to-arduino command)}
-       command))
+(defn bytes->num
+   [data]
+   (reduce bit-or
+           (map-indexed
+              (fn [i x]
+                 (bit-shift-left
+                    (bit-and x 0x0FF)
+                    (* 8
+                       (-
+                          (count data)
+                          i 1))))
+              data)))
 
 (defn handle-new-message
-  "Handles a new incoming message that was sent by the GUI."
-  [^String message]
-  (let [tokens (string/split message (Pattern/compile " "))
-        command (first tokens)
-        args (rest tokens)]
-    ((lookup-command-handler command) args)))
+   "Handles a new incoming message that was sent by the GUI."
+   [bytes]
+   {:pre [(seq? bytes)]}
+   (let
+      [command (first bytes)
+       args    (rest bytes)]
+      (case command
+         16 (println args)
+         17 (-> args byte-array String. println)
+         ;default
+         (println "Received unknown message"))))
 
-(defn handle-new-connection
-  "Handles a new connection request sent by a TCP client."
-  [stream _]
-  (s/consume
-    (fn [bytes]
-      (-> bytes
-          (byte-streams/convert String)
-          handle-new-message))
-    stream))
+(defn make-connection-handler [messageHandler clientStreams]
+   {:pre  [(ifn? messageHandler)
+           (seq? clientStreams)]
+    :post [(ifn? %)]}
+   (fn [stream _]
+      {:post [(contains? clientStreams stream)]}
+      (do
+         (dosync
+            (alter clientStreams conj stream))
+         (io!
+            (s/consume-async
+               (fn [bytes]
+                  (-> bytes
+                      byte-streams/to-byte-array
+                      seq
+                      messageHandler))
+               stream)))))
 
-(defn -main
-  "Starts the TCP server and creates Arduino
-  reader (Scanner) and printer (PrintWriter) agents."
-  []
-  (let [arduino (-> (CommPortIdentifier/getPortIdentifier arduino-com-port)
-                    (.open "Arduino Comms" 2000))]
-    (swap! is-alive (fn [_] true))
-    (def arduino-printer
-      (-> ^CommPort arduino
-          .getOutputStream
-          (PrintWriter. true)
-          agent))
-    (def arduino-reader
-      (-> ^CommPort arduino
-          .getInputStream
-          Scanner.
-          agent))
-    (send arduino-reader (fn [^Scanner reader]
-                           (loop []
-                             (when (.hasNextLine reader)
-                               (-> reader
-                                   .nextLine
-                                   handle-new-report))
-                             (recur)))))
-  (aleph.tcp/start-server handle-new-connection {:port 2401}))
+(defn -main []
+   (let
+      [clientStreams (ref [])
+       handler       (make-connection-handler handle-new-message clientStreams)]
+      (aleph.tcp/start-server handler {:port 2401})))
